3cf3d7991be07d02fd207d98f3a0a88e980f2d34
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-09 20:08:48 +0700

    start

--- server/src/startDatabase.js
+++ server/src/startDatabase.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 function getDatabaseUri() {
     if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/mean1811-test';
-    if (process.env.NODE_ENV === 'production') return 'mongodb://mean-fff:123321@ds241737.mlab.com:41737/mean1811';
+    if (process.env.NODE_ENV === 'production') return 'mongodb://abcd:xyz@ds229549.mlab.com:29549/mean1812';
     return 'mongodb://localhost/mean1811';
 }
 
